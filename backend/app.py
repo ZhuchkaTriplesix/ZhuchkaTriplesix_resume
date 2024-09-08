44c4b6a993716a21ae02237a12fa258574c77d87
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2024-09-08 23:09:58 +0300

    style

--- backend/app.py
+++ backend/app.py
@@ -8,30 +8,31 @@ import random
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Разрешаем запросы от всех доменов
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Разрешаем все HTTP методы
-    allow_headers=["*"],  # Разрешаем все заголовки
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Настройка подключения к MongoDB
 client = MongoClient('mongodb://mongo:27017/')
 db = client['comments_db']
 collection = db['comments']
+
 
 class Comment(BaseModel):
     username: str
     text: str
 
+
 @app.get("/comments", response_model=List[Comment])
 async def get_comments():
-    all_comments = list(collection.find({}, {'_id': 0}))  # Получаем все комментарии
+    all_comments = list(collection.find({}, {'_id': 0}))
     if len(all_comments) > 3:
-        random_comments = random.sample(all_comments, 3)  # Выбираем случайные 3 комментария
+        random_comments = random.sample(all_comments, 3)
     else:
-        random_comments = all_comments  # Если меньше 3, возвращаем все
-
+        random_comments = all_comments
     return random_comments
+
 
 @app.post("/comments")
 async def post_comment(comment: Comment):
